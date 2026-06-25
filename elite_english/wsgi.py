f9a2fa076c58c26e80d0c777fdf25155237218cc
taw-ssif26 <taw.ssif26@gmail.com>
2026-06-25 11:49:00 +0600

    Update wsgi.py

--- elite_english/wsgi.py
+++ elite_english/wsgi.py
@@ -1,6 +1,9 @@
+# elite_english/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elite_english.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
