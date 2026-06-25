337e199e22b5c02c0ffba573465e60360b1e42c7
taw-ssif26 <taw.ssif26@gmail.com>
2026-06-25 10:28:36 +0600

    Enable serving of media files in development
    Added static and media file serving in development mode.

--- elite_english/urls.py
+++ elite_english/urls.py
@@ -1,3 +1,4 @@
+# elite_english/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -8,6 +9,7 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+# ✅ Serve static and media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
